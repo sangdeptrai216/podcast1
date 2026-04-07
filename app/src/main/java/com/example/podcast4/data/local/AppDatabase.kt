92d938cda4045f5765fc325ab97d48923cdc62d3
Chu Hồng Sáng <chuhongsang2005@gmail.com>
2026-04-07 16:40:27 +0700

    Delete app directory

--- app/src/main/java/com/example/podcast4/data/local/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.podcast4.data.local
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [PodcastEntity::class, EpisodeEntity::class], version = 5, exportSchema = false)
-abstract class AppDatabase : RoomDatabase() {
-    abstract val podcastDao: PodcastDao
-}
